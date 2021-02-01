2d00d0ad8c0d20a2b3386669a7fa07a26cabf282
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-01 11:59:17 +0100

    update class test file

--- newclass.py
+++ newclass.py
@@ -1,12 +1,15 @@
+import random
+
 class Roulette():
-	def __init__(self, numbers, colours):
-		numbers = {'Red': (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36),
-					'Black': (2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35),
-					'Green': (0)
-					}
+	def __init__(self, result):
+		result = {(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36): 'Red',
+				(2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35): 'Black',
+				0: 'Green',
+				}
 
-		self.numbers = numbers
-		self.colours = numbers.keys()
+		self.result = result
+		
 
-Draw = Roulette('Red', 'Black')
-print(Draw.colours)
+result = random.randint(0, 35)
+Draw = Roulette(1)
+print(Draw.result)
