db10fb8a78d6b5e937b3818bd6ff4fda8e8d6bad
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-02 11:44:50 +0100

    another update to class

--- newclass.py
+++ newclass.py
@@ -1,24 +1,20 @@
 from random import randint
 #import random integer function from random module
+wheel = {'Red':[1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+				'Black':[2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
+                'Green':[0],
+				}
 
 class Roulette():
 	
 	def __init__(self, outcomes=37):
 		self.outcomes = outcomes	
-
-
+		
+	
 	def roll(self):
 	    return randint(0, self.outcomes-1)
 	    
 
-	def assign_outcome(self, x):
-		self.x = x
-		wheel = {'Red':[1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
-				'Black':[2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
-	            'Green':[0],
-					}
-	
-	
 	def store_results(self, results):
 		# Make some rolls, and store the results in a list.
 		results = []
@@ -28,15 +24,28 @@ class Roulette():
 		    results.append(result)
 		return results
 
-
+	def check_even_odd(self, result):
+		#check if the result was odd or even and return the result
+		self.result = result
+		if result % 2 == 0:
+			return True
+		else:
+			return False
+	def check_red_black_green(self, result):
+		self.result = result
+		for c, l in wheel.items():
+			for n in l:
+				if n == result:
+					return c
+		
 
 R = Roulette()
 x = (R.roll())
-#print(x)
-#assign_outcome(x)
-#print(R.store_results())
 
-#store_results()
+#print(R.store_results(x))
+#print(R.check_even_odd(x))
+print(R.check_red_black_green(x))
+
 
 """   
 # Analyze the results.
@@ -46,10 +55,7 @@ for value in range(1, die.num_sides+1):
     frequencies.append(frequency)
 """
 
-wheel = {'Red':[1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
-				'Black':[2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
-                'Green':[0],
-				}
+
 
 for c, l in wheel.items():
     for n in l:
@@ -70,15 +76,7 @@ print(roll1)
 	
 
 
-def check_odd_even(result):
-	#check if the result was odd or even and return the result
-	odd = False
-	even = False
-	if result % 2 == 0:
-		even = True
-	else:
-		odd = True
-	return odd, even
+
 
 def check_colour():
 	if result in numbers[0]:
